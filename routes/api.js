d9ac2c6b4b2ed143ff2dd177a2464bb37979b159
MykalB <mykalb081@gmail.com>
2021-06-21 15:40:51 -0400

    Update api.js
    - Complete route functionality.

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { response } = require('express');
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
@@ -8,11 +9,70 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
+      let { puzzle } = req.body;
+      let { coordinate } = req.body;
+      let { value } = req.body;
+      coordinate = coordinate.split("");
 
+      // Check for valid puzzle.
+      if(!puzzle) return res.json({error: 'Required field missing'});
+      if(puzzle.length != 81) return res.json({ error: 'Expected puzzle to be 81 characters long' });
+      if(solver.validate(puzzle)) return res.json({error: 'Invalid characters in puzzle'});
+
+      if( !/[a-iA-I]/.test(coordinate[0])|| !/^[1-9]*$/.test(coordinate[1]) || coordinate.length > 2){
+        return res.json({error: 'Invalid coordinate'});
+      }
+      if (Number(value) > 9 || Number(value) < 1){
+        return res.json({error: 'Invalid value'});
+      }
+
+      if(value == puzzle[Number(9 * (coordinate[0].toLowerCase().charCodeAt(0) - 97) + (coordinate[1] - 1))]){
+        return res.json({ valid: true });
+      }
+
+      let isValidInRow = solver.checkRowPlacement(puzzle, coordinate[0], coordinate[1], value);
+      let isValidInCol = solver.checkColPlacement(puzzle, coordinate[0], coordinate[1], value);
+      let isValidInRegion = solver.checkRegionPlacement(puzzle, coordinate[0], coordinate[1], value);
+
+      let response = {
+        valid: isValidInRow && isValidInCol && isValidInRegion
+      };
+
+      if(!response.valid){
+        response.conflict = [];
+        if(!isValidInRow) response.conflict.push('row');
+        if(!isValidInCol) response.conflict.push('column');
+        if(!isValidInRegion) response.conflict.push('region');
+      }
+      res.json(response);
+      console.log(coordinate);
+      console.log("checker: ", solver.validate(puzzle));
+      console.log("row: ", solver.checkRowPlacement(puzzle, coordinate[0], coordinate[1]));
+      console.log("col: ", solver.checkColPlacement(puzzle, coordinate[0], coordinate[1]))
+      console.log("region: ", solver.checkRegionPlacement(puzzle, coordinate[0], coordinate[1], value))
     });
     
   app.route('/api/solve')
     .post((req, res) => {
+      let { puzzle } = req.body;
+      
+      // Check for valid puzzle.
+      if(!puzzle) return res.json({error: 'Required field missing'});
+      if(puzzle.length != 81) return res.json({ error: 'Expected puzzle to be 81 characters long' });
+      if(solver.validate(puzzle)) return res.json({error: 'Invalid characters in puzzle'});
+      
+      // Attempt to solve puzzle.
+      let solved = solver.solve(puzzle);
 
+      // Response.
+      if(!solved.includes(".")){
+        res.json({
+          solution: solved
+        });
+      } else {
+        res.json({
+          error: 'Puzzle cannot be solved'
+        });
+      }
     });
 };
