a3f6d41152c28c0edc24153faa50f8c89a5ab76f
MykalB <mykalb081@gmail.com>
2021-06-21 09:33:53 -0400

    Update sudoku-solver.js
    - Validate puzzle string
    - Valid row placement

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,10 +1,23 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    /* 
+      Check if puzzeString is of valid length and valid characters:
+        returns false if invalid,
+        returns true if valid.
+    */
+    return /^[0-9.]*$/.test(puzzleString) && puzzleString.length == 81;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    /*
+      Check if value is valid in row:
+        returns false if value already exists in row,
+        returns true if value does not exist in row.
+    */
+    let rowStart = 9 * (row.toLowerCase().charCodeAt(0) - 97);
+    let rowEnd = rowStart + 9;
+    return !puzzleString.slice(rowStart, rowEnd).includes(value);
   }
 
   checkColPlacement(puzzleString, row, column, value) {
